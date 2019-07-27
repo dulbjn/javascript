10b6b23fbf8cdca7a8b5d1bcbfca504f2b7959c7
Dul Bjn <dulbjn@gmail.com>
2019-07-28 02:42:42 +0700

    Update safeplatinum.js

--- safeplatinum.js
+++ safeplatinum.js
@@ -59,7 +59,8 @@ function showurl(datajson){
 			}
 			no++;
 		}
-		if (check == false) {
+		
+if (check == false) {
 			linktag[i].href = links[randindex] + setting.path + aesCrypto.encrypt(convertstr(linktag[i].href),convertstr('root'));
 			linktag[i].rel = "nofollow";
 			linktag[i].target = "_blank";
